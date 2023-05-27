cc15a932db05d5b3ee0a660e15797083304cd6ef
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-05-27 12:51:54 -0300

    Update build.gradle.kts

--- viafabric-mc119/build.gradle.kts
+++ viafabric-mc119/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    minecraft("com.mojang:minecraft:1.19.4-alpha.23.5.a")
+    minecraft("com.mojang:minecraft:23w05a")
     mappings("net.fabricmc:yarn:23w05a+build.7:v2")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.73.4+1.19.4")
